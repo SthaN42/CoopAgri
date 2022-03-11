3540ba834888a042076a7b83d06550d5995740e9
Kaneyo0 <tomduchesne46@gmail.com>
2022-03-11 19:31:12 +0100

    Campagnes progressBar + Delete

--- js/home.js
+++ js/home.js
@@ -1,7 +1,21 @@
-jQuery(function () {
-    $( window ).on( "load",function() {
-        console.log("test");
-        let progressbar = $( ".progressBar" );
-        progressbar.css("--progress", "70%");
-    });
+$(function() {
+
+    let progressBar = $(".progressBar");
+    let deleteButton = $(".deleteIcon");
+
+    progressBar.each(function(index, element) {     
+        let barValue = $( element ).find(".barValue").text();
+        let maxBar = $( element ).find(".maxBar").text(); 
+        let value = (barValue*100)/maxBar + "%";
+        $(element).css("--progress", value.toString());
+    });  
+    
+    
+    deleteButton.each(function(index, element) {
+        $(element).on("click", function(){
+            $(element).closest(".campagne").remove()
+        })
+    })
+    
+    
 });
