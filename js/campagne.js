5d10385da9b0bdfbaf76f0d99ffe06ae9aab7ffe
Maxime Nguyen <75279829+Squalord@users.noreply.github.com>
2022-03-11 19:31:25 +0100

    Visualisation campagne

--- js/campagne.js
+++ js/campagne.js
@@ -1,14 +1,55 @@
-this.zoom_image = function () {
+let ressources = [
+    {
+        name: "Blé"
+    }
+]
+const maxAmount = 100
+let exploitants = [
+    {
+        name: "Blé dine",
+        amount: 20
+    },
+    {
+        name: "Or J",
+        amount: 30
+    },
+    {
+        name: "Blé ro",
+        amount: 10
+    }
+]
+
+this.gaugeStatus = function (exploitants) {
+    let sumsAmount = 0
+
+    for (let index = 0; index < exploitants.length; index++) {
+        sumsAmount = sumsAmount + exploitants[index].amount
+    }
+
+    if (sumsAmount <= maxAmount) {
+        $("h1").append(
+            `<div id="gauge" style="--progress:` + sumsAmount + `%;"></div>`
+        )
+    }
+    else {
+        $("h1").append(
+            `<p id="error">ERREUR : Le nombre a été dépassé</p>`
+        )
+    }
+
+}
+
+this.details = function () {
     xOffset = 10;
     yOffset = 10;
     var texte;
-    let exploitants = ["Blé dine : 20/100","Blé dine : 20/100","Blé dine : 20/100"]
     $(".ressource").hover(
         function (e) {
             texte = $(this).attr('title');
             $(this).attr('title', '');
             $("body").append(
-                `<p id='details'> Exploitant(s) <br/>` + showExploiters(exploitants) + `</p>`
+                `<p id='details'> Exploitant(s) <br/>` +
+                showExploitants(exploitants) + `</p>`
             );
             $("#details")
                 .css("top", (e.pageY - xOffset) + "px")
@@ -28,13 +69,40 @@ this.zoom_image = function () {
     });
 };
 
-this.showExploiters = function (exploitants) {
-    exploitants = this.exploitants
-    for (let position = 0; position < exploitants.length; position++) {
-        return exploitants[position];
+this.showExploitants = function (exploitants) {
+    if (exploitants.length != 0) {
+        let exploitant = exploitants[0].name + ': '
+            + exploitants[0].amount + ' / ' + maxAmount + '<br/>'
+
+        for (let index = 1; index < exploitants.length; index++) {
+            exploitant = exploitant + (exploitants[index].name + ': '
+                + exploitants[index].amount + ' / ' + maxAmount + '<br/>');
+        }
+        return exploitant
     }
+
+}
+
+this.ressource = function () {
+    $("body").append(
+        `<div>
+        <div id="ressources">
+            <div class="ressource" title="ress">
+                <p>`+ ressources[0].name + `</p>
+                <p> 60 / 100 </p>
+                <p>TONNES</p>
+            </div>
+        </div>
+    </div>`
+    )
+}
+
+this.getRessourceName = function () {
+
 }
 
 $(document).ready(function () {
-    zoom_image();
+    gaugeStatus(exploitants);
+    details();
+    ressource();
 });
