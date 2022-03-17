6b934ca9f54aa09250241adc13fe4ff6c15e988a
Kaneyo0 <tomduchesne46@gmail.com>
2022-03-17 15:56:17 +0100

    Test push

--- js/home.js
+++ js/home.js
@@ -9,13 +9,18 @@ $(function() {
         let value = (barValue*100)/maxBar + "%";
         $(element).css("--progress", value.toString());
     });  
-    
-    
+      
     deleteButton.each(function(index, element) {
         $(element).on("click", function(){
             $(element).closest(".campagne").remove()
         })
     })
     
+    
+    $.getJSON("http://vps.e-mingo.net/coopagri/app/index.php?c=api&c=Campagne"), function(Campagnes){
+        Campagnes.data.forEach( campagne => {
+            console.log("Test");  
+        })
+    }
     
 });
