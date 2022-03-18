db4437860a2a3c76d2bba81919d1cc78e9384c16
Maxime Nguyen <75279829+Squalord@users.noreply.github.com>
2022-03-18 17:31:40 +0100

    Upgrage Campagne page

--- js/campagne.js
+++ js/campagne.js
@@ -1,32 +1,79 @@
 let ressources = {
-    ble: "Blé"
+    ble: "Blé",
+    pomme: "Pomme",
+    porc: "Porc"
 }
-const maxAmount = 100
-let exploitants = [
-    {
-        name: "Blé dine",
-        amount: 20
-    },
-    {
-        name: "Or J",
-        amount: 30
-    },
-    {
-        name: "Blé ro",
-        amount: 10
-    }
-]
+let maxAmount = 0
+
+
+let exploitants = {
+    ble: [
+        {
+            maxAmount: 100
+        },
+        {
+            name: "Blé dine",
+            amount: 20
+        },
+        {
+            name: "Or J",
+            amount: 30
+        },
+        {
+            name: "Blé ro",
+            amount: 10
+        }
+    ],
+    pomme: [
+        {
+            maxAmount: 70
+        },
+        {
+            name: "Pom yeet",
+            amount: 20
+        },
+        {
+            name: "Pomme d'or",
+            amount: 30
+        },
+        {
+            name: "Gaple",
+            amount: 10
+        }
+    ],
+    porc: [
+        {
+            maxAmount: 80
+        },
+        {
+            name: "Peppa",
+            amount: 20
+        },
+        {
+            name: "Pigguard",
+            amount: 30
+        },
+        {
+            name: "Porc no",
+            amount: 10
+        }
+    ]
+}
 
 this.gaugeStatus = function () {
     let sumsAmount = 0
 
-    for (let index = 0; index < exploitants.length; index++) {
-        sumsAmount = sumsAmount + exploitants[index].amount
+    for (const [key, value] of Object.entries(exploitants)) {
+        for (let i = 1; i < value.length; i++) {
+            sumsAmount += value[i].amount;
+        }
+        maxAmount += value[0].maxAmount
     }
 
     if (sumsAmount <= maxAmount) {
+        let pourcentAmout = sumsAmount / maxAmount * 100
         $("#gauge_icon").prepend(
-            `<div id="gauge" style="--progress:` + sumsAmount + `%;"></div>`
+            `<div id="gauge" style="--progress:` + pourcentAmout + `%;"></div>`
         )
     }
     else {
@@ -34,7 +81,6 @@ this.gaugeStatus = function () {
             `<p id="error">ERREUR : Le nombre a été dépassé</p>`
         )
     }
-
 }
 
 this.details = function () {
@@ -68,32 +114,62 @@ this.details = function () {
 };
 
 this.showExploitants = function () {
-    if (exploitants.length != 0) {
-        let exploitant = exploitants[0].name + ': '
-            + exploitants[0].amount + ' / ' + maxAmount + '<br/>'
+    if (exploitants.ble.length != 0) {
+        let exploitant = ''
 
-        for (let index = 1; index < exploitants.length; index++) {
-            exploitant = exploitant + (exploitants[index].name + ': '
-                + exploitants[index].amount + ' / ' + maxAmount + '<br/>');
+        for (const [key, value] of Object.entries(exploitants)) {
+            for (let i = 1; i < value.length; i++) {
+                exploitant = exploitant + (value[i].name + ': '
+                    + value[i].amount + ' / ' + value[0].maxAmount + '<br/>');
+            }
         }
         return exploitant
     }
 
 }
 
-this.ressource = function () {
-    $("body").append(
-        `<div>
-        <div id="ressources">
-            <div class="ressource" title="ress">
-                <p>`+ ressources.ble + `</p>
-                <p> 60 / 100 </p>
-                <p>TONNES</p>
+this.showRessources = function () {
+    for (const [key, value] of Object.entries(ressources)) {
+        $("body").append(
+            `<div>
+            <div id="ressources">
+                <div class="ressource" title="ress">
+                    <p>`+ value + `</p>
+                    <p> 60 / 100 </p>
+                    <p>TONNES</p>
+                </div>
             </div>
-        </div>
-    </div>`
-    )
+        </div>`
+        )
+    }
 }
+
+// this.showRessources = function () {
+//     for (const [key, name] of Object.entries(ressources)) {
+//         $("body").append(
+//             `<div>
+//                 <div id="ressources">
+//                     <div class="ressource" title="ress">
+//                         <p id="name">`+ name + `</p>`
+//         )
+//         for (const [key, value] of Object.entries(exploitants)) {
+//             console.log(value)
+//             $("#name").append(
+//                 `<p id="goal"> /` + value[0].maxAmount + ` </p>`
+//             )
+//             for (let i = 1; i < value.length; i++) {
+//                 $("#goal").prepend(
+//                      value[i].amount
+//                 )
+//                 .append(+ value[0].maxAmount + ` </p>
+//                 <p>TONNES</p>
+//             </div>
+//         </div>
+//     </div>`)
+//             }
+//         }
+//     }
+// }
 
 this.deleteButton = function () {
     let deleteButton = $(".deleteIcon");
@@ -107,7 +183,7 @@ this.deleteButton = function () {
 
 $(document).ready(function () {
     gaugeStatus();
-    ressource();
+    showRessources();
     deleteButton();
     details();
 });
