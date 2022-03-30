b32dd4c8791e5473fb4f6dbffbb9954cfb4bf909
Kaneyo0 <tomduchesne46@gmail.com>
2022-03-30 14:43:25 +0200

    Campagne créé dynamiquement

--- js/home.js
+++ js/home.js
@@ -1,21 +1,34 @@
 $(function () {
 
     $.getJSON("http://vps.e-mingo.net/coopagri/app/index.php?c=api&n=Campagne&where=id|1", function (response) {
-        let quantiteMax = response.result[0].previsions[0].campagne.previsions[0].quantite;
+        let campagne = $(".campagne");
+        let nomCampagne = $("<p class='titreCampagne'>");
+        let progressBar = $("<div class='progressBar'>");
+        let contenant = $("<p class='contenant'>");
+        let quantiteMax = response.result[0].previsions[0].campagne.previsions[0].quantite;        
         let valeurActuelle = "450";
         let pourcent = (valeurActuelle * 100) / quantiteMax;
-        //let campagne = JSON.stringify(response.result[0].previsions[0].campagne);
-        console.log(pourcent);
-        //console.log(campagne.previsions[0].quantite);
-        /*for (let i = 0; i < previsions.length; i++) {
-            console.log(previsions[i].quantite);
-        }
-        previsions.forEach(prevision => {
-            console.log(prevision.quantite);
-        });*/
-        $('.campagne').find(".progressBar").append("<p class='contenant'>");
-        $('.contenant').append("<span class='barValue'>" + valeurActuelle + "</span>T/<span class='maxBar'>" + quantiteMax + "</span>T");
-        $(".progressBar").each(function (index, element) {
+        let boutons = $("<div class='icons'>");
+        let modifyButton = $("<i class='fa-solid fa-pen-to-square modifyIcon'>");
+        let deleteButton = $("<i class='fa-solid fa-trash-can deleteIcon'>");
+
+        //Intitulé campagne
+        nomCampagne.append(response.result[0].libelle);
+
+        //Valeur de la barre de progression
+        contenant.append("<span class='barValue'>" + valeurActuelle + "</span>T/<span class='maxBar'>" + quantiteMax + "</span>T");
+        progressBar.append(contenant);
+
+        //Création des boutons
+        boutons.append(modifyButton).append(deleteButton);
+
+        //Ajout affichage
+        campagne.append(nomCampagne);
+        campagne.append(progressBar);
+        campagne.append(boutons);
+
+        //Calcul de la barre de recherche
+        progressBar.each(function (index, element) {
             $(element).css("--progress", pourcent.toString() + "%");    
         });
     })
@@ -23,15 +36,7 @@ $(function () {
             console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
         })
 
-    let progressBar = $(".progressBar");
     let deleteButton = $(".deleteIcon");
-
-    /*progressBar.each(function (index, element) {
-        let barValue = $(element).find(".barValue").text();
-        let maxBar = $(element).find(".maxBar").text();
-        let value = (barValue * 100) / maxBar + "%";
-        $(element).css("--progress", value.toString());
-    });*/
 
     deleteButton.each(function (index, element) {
         $(element).on("click", function () {
