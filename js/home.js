da6ca8e726ea722e40b15802e8ad062442a6030d
Kaneyo0 <tomduchesne46@gmail.com>
2022-04-01 13:58:15 +0200

    Refonte de l'accueil + Affichage de chaques campagne fonctionnel

--- js/home.js
+++ js/home.js
@@ -1,21 +1,21 @@
 $(function () {
 
-    $.getJSON("http://vps.e-mingo.net/coopagri/app/index.php?c=api&n=Campagne&where=id|1", function (response) {
+    $.getJSON("http://vps.e-mingo.net/coopagri/app/index.php?c=api&n=Campagne", function (response) {
         let indexCampagne = 0;
         
+        //Récupération de toutes les campagnes
         while (response.result[indexCampagne] != null){
             let resultat = response.result[indexCampagne];
             let listeCampagnes = $("#listeCampagnes");
-            let campagne = $("<tr class='campagne grey'>");
+            let campagne;
+            if (indexCampagne%2 == 0) {
+                campagne = $("<tr class='campagne grey'>");
+            } else {
+                campagne = $("<tr class='campagne green'>");
+            }
             let nomCampagne = $("<td class='titreCampagne'>");
             let exploitant = $("<td class='exploitant'>");
             let indexExploitant = 0;
-            /*let progressBar = $("<div class='progressBar'>");
-            let contenant = $("<p class='contenant'>");
-            let indexPrevisions = 0;
-            let quantiteMax = 0;        
-            let valeurActuelle = "450";
-            let pourcent = 0;*/
             let boutons = $("<td class='icons'>");
             let modifyButton = $("<i class='fa-solid fa-pen-to-square modifyIcon'>");
             let deleteButton = $("<i class='fa-solid fa-trash-can deleteIcon'>");
@@ -23,6 +23,7 @@ $(function () {
             //Intitulé campagne
             nomCampagne.append(resultat.libelle);
 
+            //Ajout des exploitants
             while (resultat.exploitants[indexExploitant] != null) {
                 exploitant.append(resultat.exploitants[indexExploitant].toString + " ");
                 indexExploitant++;
@@ -33,26 +34,13 @@ $(function () {
             
             //Ajout affichage
             campagne.append(nomCampagne)
-            .append(exploitant)
-            .append(boutons);
+                    .append(exploitant)
+                    .append(boutons);
             
+            //Ajout de la campagne
             listeCampagnes.append(campagne);
-            
-            //Valeur de la barre de progression
-            /*while (response.result[0].previsions[indexPrevisions] != null) {
-                let valeur = response.result[0].previsions[indexPrevisions].quantite;
-                quantiteMax += valeur;
-                indexPrevisions++;
-            }
-            contenant.append("<span class='barValue'>" + valeurActuelle + "</span>T/<span class='maxBar'>" + quantiteMax + "</span>T");
-            progressBar.append(contenant);*/
 
-            //Calcul de la barre de recherche
-            /*pourcent = (valeurActuelle * 100) / quantiteMax;;
-            progressBar.each(function (index, element) {
-                $(element).css("--progress", pourcent.toString() + "%");    
-            });*/ 
-
+            //Incrémentation de l'index de la campagne
             indexCampagne++;
         }
     })
@@ -62,6 +50,7 @@ $(function () {
 
     let deleteButton = $(".deleteIcon");
 
+    //Ecoute pour chaque bouton Delete
     deleteButton.each(function (index, element) {
         $(element).on("click", function () {
             $(element).closest(".campagne").remove()
